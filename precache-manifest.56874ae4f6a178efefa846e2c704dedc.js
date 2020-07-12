fd233e19e41d6081a547cf4b69a960c59c1825df
Natalie <kinern01@gmail.com>
2020-07-12 14:14:07 -0700

    Updates

---
+++ precache-manifest.56874ae4f6a178efefa846e2c704dedc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f42bcc490fb4cf15697968e9f282c3af",
+    "url": "/redux-blogposts/index.html"
+  },
+  {
+    "revision": "e52bac7b60c0cdba539b",
+    "url": "/redux-blogposts/static/js/2.981d1e60.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/redux-blogposts/static/js/2.981d1e60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffc86e57357de9e7d976",
+    "url": "/redux-blogposts/static/js/main.b05a25a2.chunk.js"
+  },
+  {
+    "revision": "4a7ff05df567dcc17962",
+    "url": "/redux-blogposts/static/js/runtime-main.88a09887.js"
+  }
+]);
